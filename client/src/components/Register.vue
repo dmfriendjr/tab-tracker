735942dcd1227b0a19913ad49a5b7512d0ad8971
dmfriendjr <davidmfriendjr@gmail.com>
2018-01-05 14:23:15 -0500

    Setup a registration page and sending requests to node server

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<h1>Register</h1>
+
+		<input type="email"
+			name="email"
+			placeholder="email"
+			v-model="email"/>
+		<input 
+			type="password"
+			name="password"
+			placeholder="password"
+			v-model="password"/>
+		<button v-on:click="register">Register</button>
+	</div>
+</template>
+
+<script>
+import AuthenticationService from '../services/AuthenticationService';
+
+export default {
+	name: 'Register',
+	data () {
+		return {
+			email: '',
+			password: ''
+		};
+	},
+	methods: {
+		async register () {
+			const response = await AuthenticationService.register({
+				email: this.email,
+				password: this.password
+			});
+			console.log(response.data);
+		}
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
